a55719f88e0a375947a19785b9ead9a4d0dce3be
Mahesh Narasimhan <maheshya@gmail.com>
2016-12-10 15:11:38 -0500

    First Version Grid and Maps activity

---
+++ app/src/main/java/com/example/android/wesport/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.android.wesport;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    GridView androidGridView;
+    String[] gridViewString = {
+            "Basketball", "Cricket", "Football", "Tennis", "Frisbee", "Pingpong", "Soccer", "Volleyball"
+    } ;
+    int[] gridViewImageId = {
+            R.drawable.basketball, R.drawable.cricket, R.drawable.football,R.drawable.tennis,
+            R.drawable.frisbee, R.drawable.pingpong, R.drawable.soccer,R.drawable.volleyball
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomGridViewActivity adapterViewAndroid = new CustomGridViewActivity(MainActivity.this, gridViewString, gridViewImageId);
+        androidGridView = (GridView) findViewById(R.id.grid_view_image_text);
+        androidGridView.setAdapter(adapterViewAndroid);
+        androidGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int i, long id) {
+                Toast.makeText(MainActivity.this, "GridView Item: " + gridViewString[+i], Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        return true;
+    }
+    //respond to menu item selection
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+        if (item.getItemId()==R.id.menu_next) {
+           Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+}
