f4fc6617476b4d6b57e1f7504d00d5ed42021d0d
Mahesh Narasimhan <maheshya@gmail.com>
2016-12-16 16:29:18 -0500

    Completed Google Places API parsing and Markers activity

--- app/src/main/java/com/example/android/wesport/MainActivity.java
+++ app/src/main/java/com/example/android/wesport/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.android.wesport;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -25,31 +28,31 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity  implements GoogleApiClient.ConnectionCallbacks,
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private final String TAG = "LocationActivity";
 
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
-    private LocationManager locationManager;
 
     String lat, lon;
     Location mLastLocation;
     GridView androidGridView;
     String[] gridViewString = {
             "Basketball", "Cricket", "Football", "Tennis", "Frisbee", "Pingpong", "Soccer", "Volleyball"
-    } ;
+    };
     int[] gridViewImageId = {
-            R.drawable.basketball, R.drawable.cricket, R.drawable.football,R.drawable.tennis,
-            R.drawable.frisbee, R.drawable.pingpong, R.drawable.soccer,R.drawable.volleyball
+            R.drawable.basketball, R.drawable.cricket, R.drawable.football, R.drawable.tennis,
+            R.drawable.frisbee, R.drawable.pingpong, R.drawable.soccer, R.drawable.volleyball
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         CustomGridViewActivity adapterViewAndroid = new CustomGridViewActivity(MainActivity.this, gridViewString, gridViewImageId);
         androidGridView = (GridView) findViewById(R.id.grid_view_image_text);
         androidGridView.setAdapter(adapterViewAndroid);
@@ -61,28 +64,76 @@ public class MainActivity extends AppCompatActivity  implements GoogleApiClient.
                 Toast.makeText(MainActivity.this, "GridView Item: " + gridViewString[+i], Toast.LENGTH_LONG).show();
             }
         });
-//setup GoogleApiclient
-
+        Log.d(TAG, "Test location ..............");
+        //Check for permission
+        checkAppPermission();
+        //setup GoogleApiclient
         buildGoogleApiClient();
-//Check Run time permissions
-        if (BuildConfig.VERSION_CODE <23 && ContextCompat.checkSelfPermission
-                (this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            //Request for Permission
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Display UI and wait for user interaction
-            } else {
-                ActivityCompat.requestPermissions(
-                        this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                        1);
-            }
-        } else {
-            // permission has been granted, continue as usual
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        Log.d(TAG, "After build api client");
+    }
+    public void checkAppPermission(){
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission already granted.", Toast.LENGTH_LONG).show();
 
+        } else {
+            requestPermission();
+            Toast.makeText(this, "Please request permission.", Toast.LENGTH_LONG).show();
         }
-        storeprefs();
+    }
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION);
+        Log.d(TAG, "Test location 2..............");
+        return result == PackageManager.PERMISSION_GRANTED;
+
+    }
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
+        Log.d(TAG, "requestPermissions..............");
+    }
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //Checking the request code of our request
+        Log.d(TAG, "onRequestPermissionsResult..............");
+
+        if (requestCode == 1) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                //Displaying a toast
+                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        showMessageOKCancel("You need to allow access to both the permissions",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                            requestPermissions(new String[]{ACCESS_FINE_LOCATION,},
+                                                    1);
+                                        }
+                                    }
+                                });
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -91,6 +142,8 @@ public class MainActivity extends AppCompatActivity  implements GoogleApiClient.
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+        Log.d(TAG, "GoogleAPIclient init ..............");
+
     }
 
     @Override
@@ -107,72 +160,36 @@ public class MainActivity extends AppCompatActivity  implements GoogleApiClient.
         // Disconnecting the client invalidates it.
         mGoogleApiClient.disconnect();
         Log.d(TAG, "onStop fired ..............");
-
         super.onStop();
     }
 
-
     @Override
     public void onConnected(Bundle bundle) {
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            //Execute location service call if user has explicitly granted ACCESS_FINE_LOCATION..
 
-        mLocationRequest = LocationRequest.create();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(1000); // Update location every 10 seconds
-
-
-        try {
+            mLocationRequest = LocationRequest.create();
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            mLocationRequest.setInterval(100); // Update location every 10 seconds
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                     mGoogleApiClient);
-
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED) {
-                //Request for Permission
-                // ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        android.Manifest.permission.ACCESS_FINE_LOCATION)) {
-                    // Display UI and wait for user interaction
-
-                } else {
-                    ActivityCompat.requestPermissions(
-                            this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                            1);
-                }
-            } else {
-                // permission has been granted, continue as usual
-                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-
+            Log.d(TAG, "" + mLastLocation);
+            if (mLastLocation != null) {
+                lat = String.valueOf(mLastLocation.getLatitude());
+                lon = String.valueOf(mLastLocation.getLongitude());
             }
-        } catch (SecurityException e)
-            {
-                Log.e("Location Update",e+" ");
-            }
-
-        if (mLastLocation != null) {
-            lat = String.valueOf(mLastLocation.getLatitude());
-            lon = String.valueOf(mLastLocation.getLongitude());
-        }
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if ((ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission
-                    (this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
-                Location myLocation =
-                        LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-     //           Log.i(TAG, myLocation.toString());
-            }
+            Log.d(TAG, "" + lat);
+            Log.d(TAG, "" + lon);
+            Log.d(TAG, "calling storeprefs inside onconnected");
+            storeprefs(lat, lon);
         }
     }
+
     @Override
     public void onConnectionSuspended(int i) {
         Log.i(TAG, "GoogleApiClient connection has been suspend");
-        mGoogleApiClient.connect();
     }
 
     @Override
@@ -189,7 +206,9 @@ public class MainActivity extends AppCompatActivity  implements GoogleApiClient.
         // New location has now been determined
         String lat = Double.toString(location.getLatitude());
         String lon = Double.toString(location.getLongitude());
-        storeprefs();
+        Log.i(TAG, lat);
+        Log.i(TAG, lon);
+        storeprefs(lat,lon);
         // You can now create a LatLng Object for use with maps
         //LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
 
@@ -206,13 +225,13 @@ public class MainActivity extends AppCompatActivity  implements GoogleApiClient.
         super.onOptionsItemSelected(item);
         if (item.getItemId()==R.id.menu_next) {
             Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
-           //intent.putExtra(mCurrentLocation,"Current Location");
+            //intent.putExtra(mCurrentLocation,"Current Location");
             startActivity(intent);
         }
         return true;
     }
 
-    void storeprefs() {
+    void storeprefs(String lat, String lon) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("latitude", lat);
@@ -221,5 +240,6 @@ public class MainActivity extends AppCompatActivity  implements GoogleApiClient.
         String mLat = prefs.getString("latitude","");
         String mLon = prefs.getString("longtitude","");
         Log.i(TAG, mLat);
+        Log.i(TAG, mLon);
     }
 }
--- app/src/main/java/com/example/android/wesport/MapsActivity.java
+++ app/src/main/java/com/example/android/wesport/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.wesport;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,20 +14,69 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private GoogleMap mMap;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String LOG_TAG = "GooglePlaces ";
+    //String SERVICE_URL="";
+    private static final String SERVICE_URL ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.1266246,-77.207643&\r" + "radius=5000&keyword=\"park\"&key=AIzaSyA1HjqFETCMi8rOXPtujddpnjpvcQsFYJ4\n";
+    //location=39.1266246,-77.207643&radius=500&type=\"park\"&key=AIzaSyDCJelWAYPKAev6dIaAgLIx4e19NGO3UFw\n";
+
+    private GoogleMap map;
+    private final String TAG = "MapActivity";
+
+    public static MapsActivity newInstance() {
+        MapsActivity fragment = new MapsActivity();
+        return fragment;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        Log.d(TAG, "inside create Fragment");
+        setUpMapIfNeeded();
+        DownloadTask task = new DownloadTask();
+        task.execute(SERVICE_URL);
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the
+        // map.
+        if (map == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment map = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            map.getMapAsync(this);
+            // Check if we were successful in obtaining the map.
+            if (map != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        map.getUiSettings().setZoomControlsEnabled(false);
+    }
+
+     /*   // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        Log.d(TAG, "outside create Fragment");
     }
-
+*/
 
     /**
      * Manipulates the map once available.
@@ -35,12 +88,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(GoogleMap map) {
+        Log.i(LOG_TAG ,"Populate markers for parks");
+        SharedPreferences preferences= PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        Double mLat = Double.parseDouble(preferences.getString("latitude",""));
+        Double mLon = Double.parseDouble(preferences.getString("longtitude",""));
+        Log.d(TAG, preferences.getString("latitude",""));
+        Log.d(TAG, preferences.getString("longtitude",""));
+        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLat,mLon), 5));
+        //map.addMarker(new MarkerOptions().position(new LatLng(mLat,mLon)));
+        //map.setMyLocationEnabled(true);
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            final StringBuilder json = new StringBuilder();
+            try {
+                // Connect to the web service
+                url = new URL(SERVICE_URL);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                // Read the JSON data into the StringBuilder
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        // Create markers for the city data.
+        // Must run this on the UI thread since it's a UI operation.
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            try {
+                createMarkersFromJson(result.toString());
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Error processing JSON", e);
+            }
+        }
+
+    }
+
+    void createMarkersFromJson(String json) throws JSONException {
+        // De-serialize the JSON string into an array of park objects
+        map.clear();
+        JSONObject jsonObject = new JSONObject(json);
+        String parkInfo = jsonObject.getString("results");
+        Log.d("parkname", parkInfo);
+        String latitude = "";
+        String longitude = "";
+        JSONArray jsonArray = new JSONArray(parkInfo);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            // Create a marker for each near by park in the JSON data.
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+           /* Log.e(LOG_TAG, "" +jsonArray.getJSONObject(i));
+            Log.e(LOG_TAG, "" +jsonObj.getJSONObject("geometry").getJSONObject("location").getString("lat"));
+            Log.e(LOG_TAG, "" +jsonObj.getString("name"));
+            */
+            latitude = jsonObj.getJSONObject("geometry").getJSONObject("location").getString("lat");
+            longitude = jsonObj.getJSONObject("geometry").getJSONObject("location").getString("lng");
+            double lat = Double.parseDouble(latitude);
+            double lon = Double.parseDouble(longitude);
+            //CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 12));
+            map.addMarker(new MarkerOptions()
+                    .title(jsonObj.getString("name"))
+                    .position(new LatLng(lat, lon))
+
+            );
+        }
     }
 }
