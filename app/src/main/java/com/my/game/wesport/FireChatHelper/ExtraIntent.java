b0b7eba3259c1561e9bb1abd368c1432b2bd41e4
sabeeh <mr.hussnain.it@gmail.com>
2017-05-04 17:21:25 +0500

    ExtraIntents updated

--- app/src/main/java/com/my/game/wesport/FireChatHelper/ExtraIntent.java
+++ app/src/main/java/com/my/game/wesport/FireChatHelper/ExtraIntent.java
@@ -5,11 +5,5 @@ public final class ExtraIntent {
     public static final String EXTRA_USER_ID = "EXTRA_USER_ID";
     public static final String EXTRA_RECIPIENT_ID = "EXTRA_RECIPIENT_ID";
     public static final String EXTRA_RECIPIENT_USERNAME = "EXTRA_RECIPIENT_USER_NAME";
-
-
-
-    //Other user extra intent
-    public static final String EXTRA_RECIPIENT_EMAIL = "EXTRA_RECIPIENT_EMAIL";
-    public static final String EXTRA_RECIPIENT_BIO = "EXTRA_RECIPIENT_BIO";
-
+    public static final String EXTRA_RECIPIENT_PICTURE = "EXTRA_RECIPIENT_PICTURE";
 }
