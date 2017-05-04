3ec5d50c63375923f72c82bea85dd033f27cf5ad
sabeeh <mr.hussnain.it@gmail.com>
2017-05-04 16:56:21 +0500

    unit test

--- app/src/androidTest/java/com/my/game/wesport/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/my/game/wesport/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.android.wesport", appContext.getPackageName());
+        assertEquals("com.my.game.wesport", appContext.getPackageName());
     }
 }
