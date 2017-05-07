af152640baca1427fc1c66aeb534c2c8352cbfcc
sabeeh <mr.hussnain.it@gmail.com>
2017-05-07 18:05:12 +0500

    project files updated

---
+++ app/src/main/java/com/my/game/wesport/ui/FullScreenDialog.java
@@ -0,0 +1,15 @@
+package com.my.game.wesport.ui;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by sabeeh on 04-May-17.
+ */
+
+public class FullScreenDialog extends Dialog {
+    public FullScreenDialog(@NonNull Context context) {
+        super(context);
+    }
+}
